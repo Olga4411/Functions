f0f1828551ce0780c9ac57fb35491a21e38a7605
Novi_ij29@itstepru.onmicrosoft.com <Novi_ij29@itstepru.onmicrosoft.com>
2021-03-02 12:00:37 +0700

    2

--- Functions/Source.cpp
+++ Functions/Source.cpp
@@ -39,66 +39,19 @@ void main()
 	const int n = 5;
 	int arr[n];
 	FillRand(arr, n);
-	//Заполняем массив случайными числами
-	/*for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand();*/// Функция rand () возвращает псевдослучайное число в диапфзоне от 0 до 32767	}
-	// Вывод массива на экран
 	Print(arr, n);
-	/*for (int i = 0; i < n; i++)
-		{
-			cout << arr[i] << tab;
-		}
-		cout << endl;*/
 		// Сдвиг массива на заданное число элементов
 		int number_of_shifts;
 		cout << "Введите количество сдвигов:"; cin >> number_of_shifts;
 		ShiftLeft(arr, n, number_of_shifts);
-		//for (int i = 0; i < number_of_shifts; i++)//Этот цикл сдвигает массив на number_of_shifts элементов
-		//{
-		//	int buffer = arr[0];
-		//	for (int j = 0; j < n; j++)
-		//	{
-		//		arr[j] = arr[j + 1];
-		//	}
-		//	arr[n - 1] = buffer;// в буфер ложится последний элемент
-		//}
-		// Вывод сдвинутого массива на экран
 		Print(arr, n);
-		/*for (int i = 0; i < n; i++)
-		{
-			cout << arr[i] << tab;
-		}
-		cout << endl;*/
-		//////////////////////////////////////////////
 		const int m = 8;
 		int brr[m];
 		FillRand(brr, m);
-		/*for (int i = 0; i < m; i++)
-		{
-			brr[i] = rand();
-		}*/
 		Print(brr, m);
-		/*for (int i = 0; i < m; i++)
-		{
-			cout << brr[i] << tab;
-		}*/
 		cout << "Введите количество сдвигов:"; cin >> number_of_shifts;
 		ShiftLeft(brr, m, number_of_shifts);
-		//for (int i = 0; i < number_of_shifts; i++)//Этот цикл сдвигает массив на number_of_shifts элементов
-		//{
-		//	int buffer = brr[0];
-		//	for (int j = 0; j < n; j++)
-		//	{
-		//		brr[j] = brr[j + 1];
-		//	}
-		//	brr[n - 1] = buffer;// в буфер ложится последний элемент
-		//}
 		Print(brr, m);
-		/*for (int i = 0; i < n; i++)
-		{
-			cout << brr[i] << tab;
-		}*/
 		cout << endl;
 	}
 

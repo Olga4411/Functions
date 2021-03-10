1e46e3d9796111d33465b9e55b351d43f528cc20
Novi_ij29@itstepru.onmicrosoft.com <Novi_ij29@itstepru.onmicrosoft.com>
2021-03-10 11:09:46 +0700

    2

--- Functions/Source.cpp
+++ Functions/Source.cpp
@@ -32,6 +32,48 @@ void ShiftLeft(int arr[], const int n, int number_of_shifts)
 	}
 }
 
+int Sum(int arr[],const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Avg(int arr[], const int n)
+{
+	return Sum(arr, n) / n;
+}
+
+int minValueIn (int arr[],const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)
+		{
+		min = arr[i];
+		}
+	}
+	return min;
+}
+
+int maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -40,19 +82,25 @@ void main()
 	int arr[n];
 	FillRand(arr, n);
 	Print(arr, n);
-		// Сдвиг массива на заданное число элементов
-		int number_of_shifts;
+	cout << " Сумма элементов массива:" << Sum(arr, n) << endl;
+	cout << " Среднее арифметическое:" << Avg(arr, n) << endl;
+	cout << " Минимальное значение в массиве:" << minValueIn(arr, n) << endl;
+	cout << " Максимальное значение в массиве:" << maxValueIn(arr, n) << endl;
+
+		
+	// Сдвиг массива на заданное число элементов
+		/*int number_of_shifts;
 		cout << "Введите количество сдвигов:"; cin >> number_of_shifts;
 		ShiftLeft(arr, n, number_of_shifts);
-		Print(arr, n);
-		const int m = 8;
+		Print(arr, n);*/
+		/*const int m = 8;
 		int brr[m];
 		FillRand(brr, m);
 		Print(brr, m);
 		cout << "Введите количество сдвигов:"; cin >> number_of_shifts;
 		ShiftLeft(brr, m, number_of_shifts);
 		Print(brr, m);
-		cout << endl;
+		cout << endl;*/
 	}
 
 
